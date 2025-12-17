14ee9a6c99725626b8ffa46aad811e41b84b7018
Andre Barros <andrebarros7201@gmail.com>
2025-12-17 20:17:24 +0000

    chore: add Swagger support for API documentation in development environment

--- backend/Domus/Domus.API/Program.cs
+++ backend/Domus/Domus.API/Program.cs
@@ -9,12 +9,19 @@ public class Program {
 
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
+        
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
+        
         if (app.Environment.IsDevelopment()) {
-            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Domus.API v1");
+                c.RoutePrefix = "";
+            });
         }
 
         app.UseHttpsRedirection();
