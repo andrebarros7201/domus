e8e2a680b3cb141adf7a550d4fc397e5ecd6afae
Andre Barros <andrebarros7201@gmail.com>
2025-12-18 20:36:27 +0000

    feat: configure PostgreSQL with AppDbContext in Domus.API

--- backend/Domus/Domus.API/Program.cs
+++ backend/Domus/Domus.API/Program.cs
@@ -1,3 +1,5 @@
+using Domus.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi;
 
 namespace Domus.API;
@@ -9,14 +11,14 @@ public class Program {
         builder.Services.AddAuthorization();
         builder.Services.AddAuthentication();
         builder.Services.AddControllers();
-        builder.Services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Domus.API", Version = "v1" });
-            }
+        builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Domus.API", Version = "v1" }); }
         );
         builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
         var app = builder.Build();
-        
+
         if (app.Environment.IsDevelopment()) {
             app.UseSwagger();
             app.UseSwaggerUI(c => {
