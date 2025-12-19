2a0c14aedd8fa048204bfb3abb55c5d6071e78e5
Andre Barros <andrebarros7201@gmail.com>
2025-12-19 16:13:08 +0000

    feat: add ServiceResult class and ServiceResultStatus enum to standardize API responses

---
+++ backend/Domus/Domus.API/ServiceResult/ServiceResult.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Domus.API.ServiceResult;
+
+public  class ServiceResult<T> {
+    public ServiceResultStatus Status { get; set; }
+    public  string Message { get; set; }
+    public T Data { get; set; }
+    public int? NumberPages { get; set; } // Used for pagination
+
+    public static ServiceResult<T> Ok(T data, int? numberPages = null) {
+        return new ServiceResult<T> { Data = data, Status = ServiceResultStatus.Ok, NumberPages = numberPages };
+    }
+    
+    public static ServiceResult<T> Created(T data, string message) {
+        return new ServiceResult<T> { Data = data, Status = ServiceResultStatus.Created, Message = message};
+    }
+    
+    public static ServiceResult<T> Error(string message, ServiceResultStatus status) {
+        return new ServiceResult<T> { Message = message, Status = status };
+    }
+
+    public static IActionResult ServiceResultResponse(ServiceResult<T> result) {
+        return result.Status switch {
+            ServiceResultStatus.Ok => result.NumberPages != null
+                ? new OkObjectResult(new { data = result.Data, numberPages = result.NumberPages })
+                : new OkObjectResult(result.Data),
+            ServiceResultStatus.Created => new ObjectResult(new {data = result.Data}) { StatusCode = 201 },
+            ServiceResultStatus.BadRequest => new BadRequestObjectResult(new { message = result.Message }),
+            ServiceResultStatus.NotFound => new NotFoundObjectResult(new { message = result.Message }),
+            ServiceResultStatus.Conflict => new ConflictObjectResult(new { message = result.Message }),
+            ServiceResultStatus.Unauthorized => new UnauthorizedObjectResult(new { message = result.Message }),
+            ServiceResultStatus.Forbidden => new ObjectResult(new { message = result.Message }) { StatusCode = 403 },
+            _ => throw new ArgumentOutOfRangeException()
+            
+        };
+    }
+}
---
+++ backend/Domus/Domus.API/ServiceResult/ServiceResultStatus.cs
@@ -0,0 +1,11 @@
+namespace Domus.API.ServiceResult;
+
+public enum ServiceResultStatus {
+    Ok,
+    Created,
+    BadRequest,
+    NotFound,
+    Conflict,
+    Forbidden,
+    Unauthorized
+}
