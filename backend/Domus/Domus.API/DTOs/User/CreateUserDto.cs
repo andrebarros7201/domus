d74ee46b155ee5556f91ce3ba117e44cc91993e8
Andre Barros <andrebarros7201@gmail.com>
2025-12-19 20:00:07 +0000

    feat: add CreateUserDto class for user creation data transfer

---
+++ backend/Domus/Domus.API/DTOs/User/CreateUserDto.cs
@@ -0,0 +1,8 @@
+namespace Domus.API.DTOs.User;
+
+public class CreateUserDto {
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
