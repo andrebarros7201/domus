fe21aa95cbaf8d63ca8a0cbe6770606950823c96
Andre Barros <andrebarros7201@gmail.com>
2025-12-19 14:40:58 +0000

    feat: add migration to rename Property table to Properties and update relationships

---
+++ backend/Domus/Domus.API/Migrations/20251219142557_AddPropertiesTable.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domus.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertiesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Property_Users_UserId",
+                table: "Property");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Property",
+                table: "Property");
+
+            migrationBuilder.RenameTable(
+                name: "Property",
+                newName: "Properties");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Property_UserId",
+                table: "Properties",
+                newName: "IX_Properties_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Properties",
+                table: "Properties",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Properties_Users_UserId",
+                table: "Properties",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Properties_Users_UserId",
+                table: "Properties");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Properties",
+                table: "Properties");
+
+            migrationBuilder.RenameTable(
+                name: "Properties",
+                newName: "Property");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Properties_UserId",
+                table: "Property",
+                newName: "IX_Property_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Property",
+                table: "Property",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Property_Users_UserId",
+                table: "Property",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
