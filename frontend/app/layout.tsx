30b8d51e185b58c7c07b1fab1601247f6a422702
Andre Barros <andrebarros7201@gmail.com>
2025-12-29 15:51:15 +0000

    feat: integrate SidebarDesktop into RootLayout for improved navigation

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarDesktop } from "@/components/SidebarDesktop";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-row`}
       >
-        {children}
+        <SidebarDesktop />
+        <div className={"flex-1 flex-col items-center justify-start"}>
+          {children}
+        </div>
       </body>
     </html>
   );
