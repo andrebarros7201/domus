9ae268e45d159186e31e2572ffe9fecbac0efd72
Andre Barros <andrebarros7201@gmail.com>
2026-01-15 12:05:10 +0000

    fix(userSlice/login): add credentials to enable cookie creation

--- frontend/redux/slices/userSlice.ts
+++ frontend/redux/slices/userSlice.ts
@@ -46,7 +46,8 @@ export const login = createAsyncThunk<
       {
         username,
         password,
-      }
+      },
+      { withCredentials: true }
     );
     const { data } = response.data;
     return {
