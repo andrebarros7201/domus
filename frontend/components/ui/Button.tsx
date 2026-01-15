42ca5eb991e7ed56dbca4c931cae566149d3634b
Andre Barros <andrebarros7201@gmail.com>
2026-01-15 14:15:38 +0000

    feat(Button): improve style and add isDisabled prop

--- frontend/components/ui/Button.tsx
+++ frontend/components/ui/Button.tsx
@@ -2,19 +2,27 @@ type Props = {
   label: string;
   type: "button" | "submit" | "reset";
   variety: "primary" | "secondary" | "danger";
+  isDisabled?: boolean;
 };
 
-export const Button = ({ label, type, variety = "primary" }: Props) => {
+export const Button = ({
+  label,
+  type,
+  variety = "primary",
+  isDisabled = false,
+}: Props) => {
+  const varietyStyle = {
+    primary: "bg-green-800 hover:bg-green-900",
+    secondary: "bg-gray-600 hover:bg-gray-700",
+    danger: "bg-red-800 hover:bg-red-900",
+  };
+
   return (
     <button
       type={type}
-      className={`w-full text-bold text-white font-bold flex justify-center items-center p-4 hover:cursor-pointer ${
-        variety === "primary"
-          ? "bg-green-900"
-          : variety === "secondary"
-          ? "bg-gray-600"
-          : "bg-red-800"
-      }`}
+      disabled={isDisabled}
+      className={`w-full text-bold text-white font-bold flex justify-center items-center p-4 hover:cursor-pointer ${varietyStyle[variety]}
+      disabled:cursor-not-allowed disabled:bg-gray-400`}
     >
       {label}
     </button>
